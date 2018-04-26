6a10e15558e80c749ebaf33b6bb6a2bbe790023c
Jesús Alonso Abad <1956722+Kencho@users.noreply.github.com>
2018-04-26 16:10:15 +0200

    StatementPrinterPlain statement* methods visibility changed to public

--- src/ubu/gii/dass/refactoring/StatementPrinterPlain.java
+++ src/ubu/gii/dass/refactoring/StatementPrinterPlain.java
@@ -2,19 +2,19 @@ package ubu.gii.dass.refactoring;
 
 public class StatementPrinterPlain {
 
-	String statementHeader(String customerName) {
+	public String statementHeader(String customerName) {
 		return "Rental Record for " + customerName + "\n";
 	}
 
-	String statementLine(String title, double amount) {
+	public String statementLine(String title, double amount) {
 		return "\t" + title + "\t" + String.valueOf(amount) + "\n";
 	}
 
-	String statementAmountTotal(double totalAmount) {
+	public String statementAmountTotal(double totalAmount) {
 		return "Amount owed is " + String.valueOf(totalAmount) + "\n";
 	}
 
-	String statementPointsTotal(int totalFrequentRenterPoints) {
+	public String statementPointsTotal(int totalFrequentRenterPoints) {
 		return "You earned " + String.valueOf(totalFrequentRenterPoints) + " frequent renter points";
 	}
 
